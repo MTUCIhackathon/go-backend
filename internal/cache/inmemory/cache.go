09813d7e82ea32784a3602381f4e43ebe44ff8ac
Grbisba <polickarpow.dany@yandex.ru>
2025-04-11 11:22:49 +0300

    add mu and map in cache

--- internal/cache/inmemory/cache.go
+++ internal/cache/inmemory/cache.go
@@ -1,19 +1,27 @@
 package inmemory
 
 import (
+	"os"
+	"strings"
 	"sync"
 
 	"github.com/MTUCIhackathon/go-backend/internal/config"
 )
 
 type Cache struct {
-	data   sync.Map
+	mu     sync.RWMutex
+	data   map[string]string
 	config *config.Cache
 }
 
-func New(config *config.Cache) *Cache {
+func New(config *config.Cache, opts ...Options) *Cache {
+	readed, err := os.ReadFile("")
+
+	m := make(map[string]string)
+
 	return &Cache{
-		data:   sync.Map{},
+		mu:     sync.RWMutex{},
+		data:   m,
 		config: config,
 	}
 }
