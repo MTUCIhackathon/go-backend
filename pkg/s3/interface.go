0501b4eac91e7c121706c87f32c784af05f85289
Grbisba <polickarpow.dany@yandex.ru>
2025-04-15 23:43:25 +0300

    fix s3; now works; add interface

--- pkg/s3/interface.go
+++ pkg/s3/interface.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Interface interface {
-	ListObjects(ctx context.Context, bucket string) ([]string, error)
-	PutObject(ctx context.Context, bucket string, key string, data []byte) error
-	GenerateLink(ctx context.Context, bucket string, key string) (string, error)
+	ListObjects(ctx context.Context) ([]string, error)
+	PutObject(ctx context.Context, key string, data []byte) error
+	GenerateLink(ctx context.Context, key string) (string, error)
 }
--- pkg/s3/webcloud/s3.go
+++ pkg/s3/webcloud/s3.go
@@ -3,6 +3,7 @@ package webcloud
 import (
 	"bytes"
 	"context"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	awsConfig "github.com/aws/aws-sdk-go-v2/config"
@@ -12,24 +13,26 @@ import (
 	"github.com/MTUCIhackathon/go-backend/internal/config"
 )
 
+const imageKeyDir = "images"
+
 type Client struct {
 	client        *s3.Client
 	presignClient *s3.PresignClient
 	config        *config.AWS
 }
 
-func New(ctx context.Context, cfg *config.AWS) (*Client, error) {
+func New(ctx context.Context, cfg *config.Config) (*Client, error) {
 	loadAWSConfig, err := awsConfig.LoadDefaultConfig(
 		ctx,
 		awsConfig.WithCredentialsProvider(
 			credentials.NewStaticCredentialsProvider(
-				cfg.AccessKey,
-				cfg.SecretKey,
-				cfg.Region,
+				cfg.AWS.AccessKey,
+				cfg.AWS.SecretKey,
+				"",
 			),
 		),
-		awsConfig.WithBaseEndpoint(cfg.Host),
-		awsConfig.WithRegion(cfg.Region),
+		awsConfig.WithBaseEndpoint(cfg.AWS.Host),
+		awsConfig.WithRegion(cfg.AWS.Region),
 	)
 	if err != nil {
 		return nil, err
@@ -41,13 +44,13 @@ func New(ctx context.Context, cfg *config.AWS) (*Client, error) {
 	return &Client{
 		client:        client,
 		presignClient: presigned,
-		config:        cfg,
+		config:        cfg.AWS,
 	}, nil
 }
 
-func (c *Client) ListObjects(ctx context.Context, bucket string) ([]string, error) {
+func (c *Client) ListObjects(ctx context.Context) ([]string, error) {
 	objects, err := c.client.ListObjectsV2(ctx, &s3.ListObjectsV2Input{
-		Bucket: aws.String(bucket),
+		Bucket: aws.String(c.config.Bucket),
 	})
 	if err != nil {
 		return nil, err
@@ -61,22 +64,22 @@ func (c *Client) ListObjects(ctx context.Context, bucket string) ([]string, erro
 	return result, nil
 }
 
-func (c *Client) PutObject(ctx context.Context, bucket string, key string, data []byte) error {
+func (c *Client) PutObject(ctx context.Context, key string, data []byte) error {
 	_, err := c.client.PutObject(ctx, &s3.PutObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
+		Bucket: aws.String(c.config.Bucket),
+		Key:    aws.String(imageKeyDir + "/" + key),
 		Body:   bytes.NewReader(bytes.NewBuffer(data).Bytes()),
 	})
 	return err
 }
 
-func (c *Client) GenerateLink(ctx context.Context, bucket string, key string) (string, error) {
+func (c *Client) GenerateLink(ctx context.Context, key string) (string, error) {
 	input := &s3.GetObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
+		Bucket: aws.String(c.config.Bucket),
+		Key:    aws.String(imageKeyDir + "/" + key),
 	}
 	request, err := c.presignClient.PresignGetObject(ctx, input, func(opts *s3.PresignOptions) {
-		opts.Expires = c.config.LinkLifeTime
+		opts.Expires = c.config.LinkLifeTime * time.Minute
 	})
 	if err != nil {
 		return "", err
