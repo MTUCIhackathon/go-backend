cc1b0cae9292b0fcfc605f228b4024f98a195b83
Grbisba <polickarpow.dany@yandex.ru>
2025-04-16 15:02:40 +0300

    try to fix

--- pkg/pgx/after-connect.go
+++ pkg/pgx/after-connect.go
@@ -7,15 +7,17 @@ import (
 	pgxUUID "github.com/vgarvardt/pgx-google-uuid/v5"
 )
 
+const testType = "test_type"
+
 type AfterConnect func(ctx context.Context, conn *pgx.Conn) error
 
 func AddUUIDSupport(_ context.Context, conn *pgx.Conn) error {
 	pgxUUID.Register(conn.TypeMap())
 	return nil
 }
-func WithEnumTypeSupport(t string) AfterConnect {
+func WithEnumTypeSupport() AfterConnect {
 	return func(ctx context.Context, conn *pgx.Conn) error {
-		dt, err := conn.LoadType(ctx, t)
+		dt, err := conn.LoadType(ctx, testType)
 		if err != nil {
 			return err
 		}
